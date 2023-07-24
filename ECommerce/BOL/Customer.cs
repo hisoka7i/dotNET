08cdc1190ab35fc48d8c87f7998a90e291bd8547
anuja <anujabamnawat9@gmail.com>
2023-07-24 08:56:07 +0530

    Assignment lab

---
+++ ECommerce/BOL/Customer.cs
@@ -0,0 +1,7 @@
+namespace BOL.CRM;
+public class Customer{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Age{get;set;}
+}
